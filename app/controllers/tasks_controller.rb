bb5e5119964fe4e8b6658ec1e4e4c31ba4d8ae5b
Mia Sinek <mia@miasinek.com>
2023-11-10 17:03:31 +0100

    Implement Task - VIP

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,11 +25,11 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
+        # when a Turbo Stream request is sent to the create action, respond with a matching create.turbo_stream.erb file
+        format.turbo_stream
         format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
